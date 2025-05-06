bfcb2380b392dba65c705b274d8ad2ef697f36b0
SRdeMora <samuelmadrid.text@gmail.com>
2025-05-06 17:22:03 +0200

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,17 +1,16 @@
+import os
 from flask import Flask, request, jsonify
 from googletrans import Translator
-from flask_cors import CORS  # Para permitir conexiones desde el frontend
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para evitar bloqueos del navegador
+CORS(app)
 translator = Translator()
 
-# Ruta GET para verificar que el servicio está activo
 @app.route("/", methods=["GET"])
 def verificar():
     return jsonify({"mensaje": "¡El traductor está funcionando!"})
 
-# Ruta POST para traducir texto
 @app.route("/traducir", methods=["POST"])
 def traducir_texto():
     data = request.get_json()
@@ -22,5 +21,7 @@ def traducir_texto():
     texto_ingles = translator.translate(texto_espanol, src="es", dest="en").text
     return jsonify({"traduccion": texto_ingles})
 
+# Usar el puerto asignado por Render
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
